a3347899e58dff44de989741b55d165d6b0e9e59
rafik202003 <76202568+rafik202003@users.noreply.github.com>
2026-02-09 15:16:09 +0100

    Add MainForm.Designer.cs file

---
+++ Forms/MainForm.Designer.cs
@@ -0,0 +1,31 @@
+namespace ProductionSchedulingGantt.Forms
+{
+    partial class MainForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "MainForm";
+            this.Text = "Production Scheduling - Gantt Chart";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+        }
+    }
+}
